7815ff8a372919c17dbd90f7e2a48ea229daae1b
anishKateel <119728133+anishKateel@users.noreply.github.com>
2023-05-26 11:27:40 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 	
 	@GetMapping("/")
 	public String getMessage() {
-		return "Hurray anish!! openshift deployment is successful";
+		return "Hurray Bharath!! openshift deployment is successful";
 	}
 	
 	@PostMapping("/{name}")
